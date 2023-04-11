78bf50cd15e7bc79af8801be66c7a2f07505810a
sonikf <93765174+sonikf@users.noreply.github.com>
2023-04-11 23:52:13 +0300

    Add files via upload

---
+++ core/modules/facture/mod_facture_moon.php
@@ -0,0 +1,172 @@
+<?php
+/* Copyright (C) 2003-2007	Rodolphe Quiedeville		<rodolphe@quiedeville.org>
+ * Copyright (C) 2004-2011	Laurent Destailleur			<eldy@users.sourceforge.net>
+ * Copyright (C) 2005-2007	Regis Houssin				<regis.houssin@inodbox.com>
+ * Copyright (C) 2008		Raphael Bertrand (Resultic)	<raphael.bertrand@resultic.fr>
+ * Copyright (C) 2013		uanjo Menent				<jmenent@2byte.es>
+ * Copyright (C) 2022		Anthony Berton				<anthony.berton@bb2a.fr>
+ * Copyright (C) 2022-2023  Nikos Drosis                 <ndrosis@sysaid.gr>
+ * Copyright (C) 2022-2023  Nick Fragoulis
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ * or see https://www.gnu.org/
+ */
+
+/**
+ *	\file       htdocs/core/modules/facture/mod_facture_moon.php
+ *	\ingroup    facture
+ *	\brief      File containing class for numbering module Moon
+ */
+require_once DOL_DOCUMENT_ROOT.'/core/modules/facture/modules_facture.php';
+
+
+/**
+ *	Class of numbering module Moon for invoices
+ */
+class mod_facture_moon extends ModeleNumRefFactures
+{
+	/**
+	 * Dolibarr version of the loaded document
+	 * @var string
+	 */
+	public $version = 'dolibarr'; // 'development', 'experimental', 'dolibarr'
+
+	/**
+	 * @var string Error message
+	 */
+	public $error = '';
+
+
+	/**
+	 *  Returns the description of the numbering model
+	 *
+	 *  @return     string      Descriptive text
+	 */
+	public function info()
+	{
+		global $db, $langs;
+
+		$langs->load("bills");
+
+		$form = new Form($db);
+
+		$texte = $langs->trans('GenericNumRefModelDesc')."<br>\n";
+		$texte .= '<form action="'.$_SERVER["PHP_SELF"].'" method="POST">';
+		$texte .= '<input type="hidden" name="token" value="'.newToken().'">';
+		$texte .= '<input type="hidden" name="action" value="updateMask">';
+		$texte .= '<input type="hidden" name="maskconstinvoice" value="FACTURE_MOON_MASK_INVOICE">';
+		$texte .= '<input type="hidden" name="maskconstcredit" value="FACTURE_MOON_MASK_CREDIT">';
+		$texte .= '<table class="nobordernopadding" width="100%">';
+
+		$tooltip = $langs->trans("GenericMaskCodes", $langs->transnoentities("Invoice"), $langs->transnoentities("Invoice"));
+		$tooltip .= $langs->trans("GenericMaskCodes3");
+
+
+		// Setting the prefix
+		$texte .= '<tr><td><span class="opacitymedium">'.$langs->trans("Mask").' ('.$langs->trans("InvoiceStandard").'):</span></td>';
+		$texte .= '<td class="right">'.$form->textwithpicto('<input type="text" class="flat minwidth175" name="maskinvoice" value="'.getDolGlobalString("FACTURE_MOON_MASK_INVOICE").'">', $tooltip, 1, 1).'</td>';
+
+		$texte .= '<td class="left" rowspan="3">&nbsp; <input type="submit" class="button button-edit" name="Button"value="'.$langs->trans("Modify").'"></td>';
+
+		$texte .= '</tr>';
+
+		// Prefix setting of credit note
+		$texte .= '<tr><td><span class="opacitymedium">'.$langs->trans("Mask").' ('.$langs->trans("InvoiceAvoir").'):</span></td>';
+		$texte .= '<td class="right">'.$form->textwithpicto('<input type="text" class="flat minwidth175" name="maskcredit" value="'.getDolGlobalString("FACTURE_MOON_MASK_CREDIT").'">', $tooltip, 1, 1).'</td>';
+		$texte .= '</tr>';
+
+		$texte .= '</table>';
+		$texte .= '</form>';
+
+		return $texte;
+	}
+
+	/**
+	 *  Return an example of number value
+	 *
+	 *  @return     string      Example
+	 */
+	public function getExample()
+	{
+		global $conf, $langs, $mysoc;
+
+		$old_code_client = $mysoc->code_client;
+		$old_code_type = $mysoc->typent_code;
+		$mysoc->code_client = 'CCCCCCCCCC';
+		$mysoc->typent_code = 'TTTTTTTTTT';
+		$numExample = $this->getNextValue($mysoc, '');
+		$mysoc->code_client = $old_code_client;
+		$mysoc->typent_code = $old_code_type;
+
+		if (!$numExample) {
+			$numExample = 'NotConfigured';
+		}
+		return $numExample;
+	}
+
+	/**
+	 * Return next value
+	 *
+	 * @param	Societe		$objsoc     Object third party
+	 * @param   Facture		$invoice	Object invoice
+	 * @param   string		$mode       'next' for next value or 'last' for last value
+	 * @return  string      			Value if OK, 0 if KO
+	 */
+	public function getNextValue($objsoc, $invoice, $mode = 'next')
+	{
+		global $db;
+
+		require_once DOL_DOCUMENT_ROOT.'/core/lib/functions2.lib.php';
+
+		// Get Mask value
+		$mask = '';
+        if (is_object($invoice) && $invoice->type == 2) {
+			$mask = getDolGlobalString('FACTURE_MOON_MASK_CREDIT');
+		} else {
+			$mask = getDolGlobalString('FACTURE_MOON_MASK_INVOICE');
+		}
+		if (!$mask) {
+			$this->error = 'NotConfigured';
+			return 0;
+		}
+
+		$where = '';
+		//if ($facture->type == 2) $where.= " AND type = 2";
+		//else $where.=" AND type != 2";
+
+		// Get entities
+		$entity = getEntity('invoicenumber', 1, $invoice);
+
+		$numFinal = get_next_value($db, $mask, 'facture', 'ref', $where, $objsoc, $invoice->date, $mode, false, null, $entity);
+		if (!preg_match('/([0-9])+/', $numFinal)) {
+			$this->error = $numFinal;
+		}
+
+		return $numFinal;
+	}
+
+
+	/**
+	 * Return next free value
+	 *
+	 * @param	Societe		$objsoc     	Object third party
+	 * @param	string		$objforref		Object for number to search
+	 * @param   string		$mode       	'next' for next value or 'last' for last value
+	 * @return  string      				Next free value
+	 */
+	public function getNumRef($objsoc, $objforref, $mode = 'next')
+	{
+		return $this->getNextValue($objsoc, $objforref, $mode);
+	}
+}
